887489aa54d62ef7968816e8cc12f113b273c815
Surabhi Jaiswal <jaiswalsurabhi.nz4@gmail.com>
2024-02-19 17:21:36 +1300

    commiting runner

--- src/test/java/com/surabhi/test/runner/TestRunner.java
+++
@@ -1,9 +0,0 @@
-package com.surabhi.test.runner;
-
-
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features = {"src/test/java/com/surabhi/test/features/LinkedInResumeUpload.feature"}, glue = {"com/surabhi/test/StepDef"})
-public class TestRunner {
-
-}
